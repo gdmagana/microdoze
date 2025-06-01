baba9d40d9e5861960514c37f88df719ff6aa68b
aclorgen <115695565+dreasthecoder@users.noreply.github.com>
2025-05-31 17:18:31 -0700

    narrative polish

--- control.gd
+++ control.gd
@@ -2,7 +2,7 @@ extends Control
 
 func _on_start_button_pressed():
 	# Start button should lead to the main game
-	get_tree().change_scene_to_file("res://scenes/narrative_one.tscn")
+	get_tree().change_scene_to_file("res://scenes/narrative_2.tscn")
 
 func _on_quit_button_pressed():
 	get_tree().quit()
